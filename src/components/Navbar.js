f7b41c6738168c1cb4087d57e4beb911bc41b63b
Pankaj Singh Parihar <pankajsinghparihar211@gmail.com>
2022-11-01 23:17:53 +0530

    things done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,68 +1,63 @@
+import React from "react";
 
-import React from 'react';
-
-import { connect } from 'react-redux';
-import {addMoviesToList,handleMovieSearch} from '../actions'
-
+import { connect } from "react-redux";
+import { addMoviesToList, handleMovieSearch } from "../actions";
 
 class Navbar extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      showSearchResult :true,
-      searchText:''
+      showSearchResult: true,
+      searchText: "",
     };
   }
 
-
-  handleAddToMovies=(movie)=>{
+  handleAddToMovies = (movie) => {
     this.props.dispatch(addMoviesToList(movie));
     this.setState({
-      showSearchResult:false
+      showSearchResult: false,
     });
-  }
+  };
 
-  handleSearch=()=>{
-
-    const {searchText}=this.state;
+  handleSearch = () => {
+    const { searchText } = this.state;
     this.props.dispatch(handleMovieSearch(searchText));
   };
 
-  handleChange =(e)=>{
+  handleChange = (e) => {
     this.setState({
-      searchText: e.target.value
-    })
+      searchText: e.target.value,
+    });
+  };
 
-  }
+  render() {
+    const { result: movie, showSearchResult } = this.props.search;
 
-  render(){
-    
-    const {result:movie,showSearchResult} = this.props.search;
-    
-  return (
-    <div className="nav">
-     <div className='search-container'>
-            <input onChange={this.handleChange}/>
-            <button id='search-btn' onClick={this.handleSearch}>Search</button>
+    return (
+      <div className="nav">
+        <div className="search-container">
+          <input onChange={this.handleChange} />
+          <button id="search-btn" onClick={this.handleSearch}>
+            Search
+          </button>
 
-            {showSearchResult && 
-               <div className='search-results'>
-                <div className='search-result'>
-                  <img src = {movie.Poster} alt="search-pic" />
-                    <div className='movie-info'>
-                      <span>{movie.Title}</span>
-                      <button onClick={()=> this.handleAddToMovies(movie)}>Add to Movies</button>
-                      </div>
-
-                  </div>
+          {showSearchResult && (
+            <div className="search-results">
+              <div className="search-result">
+                <img src={movie.Poster} alt="search-pic" />
+                <div className="movie-info">
+                  <span>{movie.Title}</span>
+                  <button onClick={() => this.handleAddToMovies(movie)}>
+                    Add to Movies
+                  </button>
                 </div>
-
-            }
-     </div>
-    </div>
-  );
- }
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 // class NavbarWrapper extends React.Component{
@@ -77,12 +72,10 @@ class Navbar extends React.Component {
 //   }
 // }
 
-
-function MapStateToState({search}){
-   return {
+function MapStateToState({ search }) {
+  return {
     search,
-
-   }
+  };
 }
 
 export default connect(MapStateToState)(Navbar);
